ee21209fca33fda0badfc6c4cbc91884165b6af2
Ivan Necerini <ivan.necerini@gmail.com>
2025-07-08 18:15:38 +0200

    FINE ANALISI RUMORE

--- 2_Control/EXAM_PROJECT/Project/utils/params.m
+++ 2_Control/EXAM_PROJECT/Project/utils/params.m
@@ -2,17 +2,17 @@ function p = params(topology_name)
 % PARAMS returns the general configuration parameters for the distributed maglev control project.
 
     if nargin < 1
-        topology_name = 'line';  % default
+        topology_name = 'full';  % default
     end
 
     % Number of follower agents
     p.N = 6;  % Total number of followers (6 agents)
 
     % Controller parameters
-    p.c = 2;  % Coupling gain for distributed control
+    p.c = 3.4272;  % Coupling gain for distributed control
 
     % Lyapunov observer design parameters
-    p.Q = eye(2)*10;  % State weighting matrix
+    p.Q = eye(2);  % State weighting matrix
     p.R = 1;       % Input weighting (scalar), as in reference code
 
     % Pinning matrix: agent S1 is the only pinned leader
@@ -31,7 +31,7 @@ function p = params(topology_name)
     p.scelta_riferimento = 'step';  % Choose input type: 'step', 'ramp', or 'sin'
 
     % Noise configuration
-    p.noise_sensitivity = 0.1;  % Global noise scaling for the leader
-    p.agent_noise_sensitivity_vector = [0.1 0 0.1 0 0 0.2];  % Per-agent noise levels
+    p.noise_sensitivity = 0;  % Global noise scaling for the leader
+    p.agent_noise_sensitivity_vector = [0.5 0.5 0.5 0.5 0.5 0.5];  % Per-agent noise levels
 
 end
